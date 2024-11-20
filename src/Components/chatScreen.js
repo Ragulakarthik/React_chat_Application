54a2b49e4c34af8cb9093662a081a37bd96bbee5
Karthik Ragula <ragulakarthik04@gmail.com>
2024-11-20 23:04:26 +0530

    updated css

--- src/Components/chatScreen.js
+++ src/Components/chatScreen.js
@@ -18,9 +18,17 @@ function ChatScreen() {
 
     return (
         <div className="chat-screen">
-            <div>
-                <a href='./questions' >&lt;</a>  <span className='questionHead'>QUESTIONS</span>
+            <div className='questionHeadDiv'>
+                <a href='./questions'>
+                    <svg fill="#000000" width="15px" height="15px" viewBox="0 0 52 52" xmlns="http://www.w3.org/2000/svg">
+                        <g>
+                            <path d="M38,52a2,2,0,0,1-1.41-.59l-24-24a2,2,0,0,1,0-2.82l24-24a2,2,0,0,1,2.82,0,2,2,0,0,1,0,2.82L16.83,26,39.41,48.59A2,2,0,0,1,38,52Z" />
+                        </g>
+                    </svg>
+                </a>
+                <span className='questionHead'>QUESTIONS</span>
             </div>
+
             {questionData && (
                 <>
                     <div className="questionDiv">
@@ -30,7 +38,7 @@ function ChatScreen() {
                         {messages.map((msg, index) => (
                             <div key={index} className="message-wrapper">
                                 {msg.sender === 'User' && (
-                                    <img src= {userPic} alt="User" className="user-image" />
+                                    <img src={userPic} alt="User" className="user-image" />
                                 )}
                                 <div className={`message ${msg.sender.toLowerCase()}`}>
                                     <span>{msg.text}</span>
@@ -47,7 +55,11 @@ function ChatScreen() {
                             placeholder="Type your message"
                         />
                         <button onClick={handleSendMessage} className="send-button">
-                            &gt;
+                            <svg fill="#000000" width="10px" height="10px" viewBox="0 0 52 52" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+                                <g id="SVGRepo_iconCarrier"> <g data-name="Group 132" id="Group_132"> <path d="M38,52a2,2,0,0,1-1.41-.59l-24-24a2,2,0,0,1,0-2.82l24-24a2,2,0,0,1,2.82,0,2,2,0,0,1,0,2.82L16.83,26,39.41,48.59A2,2,0,0,1,38,52Z" /> </g> </g>
+                            </svg>
                         </button>
                     </div>
                 </>
